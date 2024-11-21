ff25595a5fecd6dc3d274e6a1216cd95fa3b0c7b
Joe Birr-Pixton <jpixton@gmail.com>
2024-11-21 20:08:00 +0000

    rustls-bench: support graviola
    This introduces a patch, so that rustls-bench -> rustls is the same crate
    as rustls-bench -> rustls-graviola -> rustls.

--- rustls-bench/src/main.rs
+++ rustls-bench/src/main.rs
@@ -962,6 +962,8 @@ enum Provider {
     AwsLcRs,
     #[cfg(all(feature = "aws-lc-rs", feature = "fips"))]
     AwsLcRsFips,
+    #[cfg(feature = "graviola")]
+    Graviola,
     #[cfg(feature = "post-quantum")]
     PostQuantum,
     #[cfg(feature = "ring")]
@@ -977,6 +979,8 @@ impl Provider {
             Self::AwsLcRs => rustls::crypto::aws_lc_rs::default_provider(),
             #[cfg(all(feature = "aws-lc-rs", feature = "fips"))]
             Self::AwsLcRsFips => rustls::crypto::default_fips_provider(),
+            #[cfg(feature = "graviola")]
+            Self::Graviola => rustls_graviola::default_provider(),
             #[cfg(feature = "post-quantum")]
             Self::PostQuantum => rustls_post_quantum::provider(),
             #[cfg(feature = "ring")]
@@ -991,6 +995,8 @@ impl Provider {
             Self::AwsLcRs => rustls::crypto::aws_lc_rs::Ticketer::new(),
             #[cfg(all(feature = "aws-lc-rs", feature = "fips"))]
             Self::AwsLcRsFips => rustls::crypto::aws_lc_rs::Ticketer::new(),
+            #[cfg(feature = "graviola")]
+            Self::Graviola => rustls_graviola::Ticketer::new(),
             #[cfg(feature = "post-quantum")]
             Self::PostQuantum => rustls::crypto::aws_lc_rs::Ticketer::new(),
             #[cfg(feature = "ring")]
@@ -1015,8 +1021,12 @@ impl Provider {
     }
 
     fn supports_key_type(&self, _key_type: KeyType) -> bool {
-        // currently all providers support all key types
-        true
+        match self {
+            #[cfg(feature = "graviola")]
+            Self::Graviola => !matches!(_key_type, KeyType::Ed25519),
+            // all other providers support all key types
+            _ => true,
+        }
     }
 
     fn choose_default() -> Self {
@@ -1028,6 +1038,9 @@ impl Provider {
 
         #[cfg(all(feature = "aws-lc-rs", feature = "fips"))]
         available.push(Self::AwsLcRsFips);
+
+        #[cfg(feature = "graviola")]
+        available.push(Self::Graviola);
 
         #[cfg(feature = "post-quantum")]
         available.push(Self::PostQuantum);
